9d2cdeda4449bd50dcfc288839a9eabfe7d2df32
YeongsikLee <leejk9592@naver.com>
2018-07-08 15:57:41 +0900

    Remove Function "strlen" To Reduce Executing Time

--- Easy/5430_AC.cpp
+++ Easy/5430_AC.cpp
@@ -1,94 +1,88 @@
-/**
-*	@link	https://www.acmicpc.net/problem/5430
-*	@date	2017. 01. 31 18:20
-*	@author	Sikurity
-*	@method Use STL Deque
-*/
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>
-#include <vector>
-#include <deque>
+#include <cstdio>
 
-#define MAX_LEN 100000
+const int MAX_CMD_LEN = 100000;
+const int MAX_LIST_LEN = 100000;
 
-using namespace std;
+char p[MAX_CMD_LEN + 1];
+char tmp[MAX_LIST_LEN * 3 + (MAX_LIST_LEN - 1) + 2];
+int list[MAX_LIST_LEN];
 
-int main()
-{
-	char p[MAX_LEN + 1], c;
-	int testcase, n, t, i, length;
-	bool isReversed, isError;
-	deque<int> DQ;
-
-	scanf("%d", &testcase);
-	while(testcase--)
-	{
-		isReversed = isError = false;
-
-		scanf("%s", p);
-		scanf("%d", &n);
-		scanf("%c%c", &c, &c);
-		for( i = 0 ; i < n ; i++ )
-		{
-			scanf("%d", &t);
-			DQ.push_back(t);
-			scanf("%c", &c);
-		}
-		scanf("%c", &c);
-
-		length = strlen(p);
-		for(i = 0 ; i < length ; i++)
-		{
-			if( p[i] == 'R' )
-				isReversed = !isReversed;
-			else
-			{
-				if( DQ.size() )
-				{
-					if(isReversed)
-						DQ.pop_back();
-					else
-						DQ.pop_front();
-				}
-				else
-				{
-					isError = true;
-					break;
-				}
-			}
-		}
-
-		if( isError )
-			printf("error\n");
-		else
-		{
-			printf("[");
-			if(isReversed)
-			{
-				length = DQ.size();
-				for(i = length - 1 ; i >= 0 ; i--)
-				{
-					printf("%d", DQ[i]);
-					if( i )
-						printf(",");
-				}
-			}
-			else
-			{
-				length = DQ.size();
-				for(i = 0 ; i < length ; i++)
-				{
-					printf("%d", DQ[i]);
-					if( i + 1 < length )
-						printf(",");
-				}
-			}
-			printf("]\n");
-		}
-
-		DQ.clear();
-	}
-
-	return 0;
+int main() {
+    int testcase, n, front, rear;
+    bool isReversed, isError;
+    
+    scanf("%d", &testcase);
+    while(testcase--) {
+        isReversed = isError = false;
+        
+        scanf("%s %d", p, &n);
+        front = 0, rear = n - 1;
+        
+        scanf("%s", tmp);
+        
+        int num = 0;
+        int digit = 0;
+        int idx = 0;
+        for(int i = 0; tmp[i]; i++ ) {
+            if(tmp[i] == '[')
+                continue;
+            if(tmp[i] == ',' || tmp[i] == ']') {
+                int mul = 1;
+                for(int j = i - 1; j >= i - digit; j--) {
+                    num += (tmp[j] - '0') * mul;
+                    mul *= 10;
+                }
+                
+                list[idx++] = num;
+                
+                num = 0;
+                digit = 0;
+            }
+            else
+                digit++;
+        }
+        
+        for(int i = 0 ; p[i] ; i++) {
+            if( p[i] == 'R' )
+                isReversed = !isReversed;
+            else {
+                if( front <= rear ) {
+                    if(isReversed)
+                        rear--;
+                    else
+                        front++;
+                }
+                else {
+                    isError = true;
+                    break;
+                }
+            }
+        }
+        
+        if( isError )
+            printf("error\n");
+        else {
+            printf("[");
+            
+            if(isReversed) {
+                for(int i = rear ; i >= front ; i--) {
+                    printf("%d", list[i]);
+                    if( front < i )
+                        printf(",");
+                }
+            }
+            else {
+                for(int i = front ; i <= rear ; i++) {
+                    printf("%d", list[i]);
+                    if( i < rear )
+                        printf(",");
+                }
+            }
+            
+            printf("]\n");
+        }
+    }
+    
+    return 0;
 }
+
